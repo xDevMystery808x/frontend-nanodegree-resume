eb56f1b2e029304f374cd781382a29f5c1297723
xDevMystery808x <Kamaile@hinas-MacBook-Air.local>
2017-12-13 12:58:26 -1000

    Added name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append("Mimi");
+//$("#main").prepend("Mimi");
+
+//var email = "mia@udacity.com";
+//var newEmail = email.replace("udacity", "gmail");
+
+//comsole.log(email);
+//console.log(newEmail);
+
+//var awesomeThoughts = "My name is Mia and Im the Best!";
+
+//var funThoughts = awesomeThoughts.replace ("Best", "Awesome");
+
+//$("#main").append(funThoughts);
+var name = "Mia Nunes";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var roleName = "Coding Instructor";
+var roleFormattedName = HTMLheaderRole.replace("%data%", roleName);
+
+$("#header").append(formattedName);
+$("#header").append(roleFormattedName);
